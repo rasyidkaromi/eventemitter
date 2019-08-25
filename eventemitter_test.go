3c168cd7efc3e54cd8e8a841e2f93fcff8a40c0d
Rasyid Karomi <waverate@gmail.com>
2019-08-25 23:54:12 +0700

    Create eventemitter_test.go

---
+++ eventemitter_test.go
@@ -0,0 +1,27 @@
+package eventemitter
+
+import	"log"
+import	"testing"
+
+
+
+func BenchmarkObject(b *testing.B) {
+	ser := New(1000000)
+	c1 := ser.On("ch1")
+	var pnt int
+	for i := 0; i < b.N; i++ {
+		ser.Emit(`{
+			"data": "data",
+			"data 2": "data 2"
+			}`, "ch1")
+
+		if val, ok := <-c1; !ok {
+			log.Println(" Error found on subscribed.")
+		} else {
+			pnt++
+			log.Println(ok)
+			log.Println("val :", val)
+			log.Println(pnt)
+		}
+	}
+}
